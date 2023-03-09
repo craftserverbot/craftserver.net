a537b5a8e0baeff7a2dba5a0a6ada8ad91ac773d
doinkythederp <doinkythederp@icloud.com>
2023-03-08 21:19:36 -0800

    refactor: use direct link to invite page

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const INVITE_LINK = "https://top.gg/bot/1044791133416464384/invite";
+export const INVITE_LINK =
+    "https://discord.com/api/oauth2/authorize?client_id=1044791133416464384&permissions=313344&scope=bot%20applications.commands";
 export const VOTE_LINK = "https://top.gg/bot/1044791133416464384/vote";
 export const SUPPORT_LINK = "https://discord.gg/9XRpc6DTg9";
