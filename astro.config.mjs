a5a8ab41c4704bf108be713616531630daf924e2
doinkythederp <doinkythederp@icloud.com>
2023-09-06 21:56:22 -0700

    feat: fix images pt 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp",
-        }),
-    ],
+    integrations: [tailwind()],
     site: "https://craftserver.net",
     vite: {
         ssr: {
