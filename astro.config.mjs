71dd2f0fcd638d635942e33cf4c997d90815a060
Lewis McClelland <lewis@lewismcclelland.me>
2025-11-15 16:10:51 -0500

    Fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import remarkToc from "remark-toc";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
     site: "https://craftserver.net",
     vite: {
         ssr: {
             external: ["@11ty/eleventy-img"],
         },
+
+        plugins: [tailwindcss()],
     },
     markdown: {
         remarkPlugins: [remarkToc],
